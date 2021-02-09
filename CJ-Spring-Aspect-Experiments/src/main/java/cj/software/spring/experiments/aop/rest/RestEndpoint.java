55b37447c7c1e37eabad0b2a23db76620bfde6cc
cjacobme <cjacobme@aol.com>
2021-02-09 20:44:05 +0100

    let AOP produce the correlation id

--- CJ-Spring-Aspect-Experiments/src/main/java/cj/software/spring/experiments/aop/rest/RestEndpoint.java
+++ CJ-Spring-Aspect-Experiments/src/main/java/cj/software/spring/experiments/aop/rest/RestEndpoint.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,19 +41,10 @@ public class RestEndpoint
 	@GetMapping(path = "{id}")
 	public ContractGetOutput getContractDetail(@PathVariable(name = "id", required = true) Long id)
 	{
-		String oldCorrelationId = MDC.get("correlationId");
-		try
-		{
-			MDC.put("correlationId", String.format("Contract %d", id));
-			this.logger.info("download contract details for %d...", id);
-			ContractDetail contractDetail = this.contractRepository.getContractDetail(id);
-			ContractGetOutput result = new ContractGetOutput(contractDetail);
-			this.logger.info("return detail");
-			return result;
-		}
-		finally
-		{
-			MDC.put("correlationId", oldCorrelationId);
-		}
+		this.logger.info("download contract details for %d...", id);
+		ContractDetail contractDetail = this.contractRepository.getContractDetail(id);
+		ContractGetOutput result = new ContractGetOutput(contractDetail);
+		this.logger.info("return detail");
+		return result;
 	}
 }
